a4d0c654fe8cef5566d87b1b6bbde100b8a45671
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-10-22 20:56:59 +0700

    .

--- DienMayDo/connect.php
+++ DienMayDo/connect.php
@@ -3,7 +3,7 @@
 	//mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);
 
-	$Connect = pg_connect("postgres://eazlcisnrkhcxl:9f53ac5bdffd1a7d15b6241cf644835010941041357401951db4dd0b2f508449@ec2-52-201-72-91.compute-1.amazonaws.com:5432/dhm6m6enahrfl");
+	$Connect = pg_connect("postgres://kkhbsaqkzdqykx:54ddd61a4283d1770df62432ed7f08b0ac42ba7ae7d9e2bb519810225fff3af3@ec2-3-212-168-103.compute-1.amazonaws.com:5432/df5bjm5q08o5k2");
 	//$Connect =pg_connect("host=localhost port=5432 dbname=postgres pass=123456")
 	if (!$Connect) {
         die("Connection failed");
